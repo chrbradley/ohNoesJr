6809874d6b536c21162851703b210cbcf225454c
Chris Bradley <chrbradley@gmail.com>
2015-03-09 14:05:15 -0400

    tweaked problem verbiage

--- helpers/filterChallenge.js
+++ helpers/filterChallenge.js
@@ -4,7 +4,7 @@ var initialText = "/*\n" +
 "filter is a function used to iterate over items of an array.\n" +
 "filter passes each item in the array to a callback function.\n\n" +
 "This callback function is sometimes refered to as a predicate or test function which just means that the function will only ever return either true or false.\n\n" +
-"If the predicate function returns true for that item, it is added to a new array. When filter is finished iterating, it will return the new array.\n\n" +
+"If the predicate function returns true an item, it is added to a new array. When filter is finished iterating, it will return the new array.\n\n" +
 "An example use case might look like this:\n\n" +
 "var numbers = [7, 2, 4, 3, 8, 1, 9];\n" +
 "filter(numbers, function(num) { if (num > 3) return true;});\n\n" +
